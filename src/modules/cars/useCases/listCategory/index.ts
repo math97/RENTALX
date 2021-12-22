4e2219464b20f6a3037773326c05c6ee01f83532
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2021-12-22 10:07:42 -0300

    modify to use db instead of  a local array

--- src/modules/cars/useCases/listCategory/index.ts
+++ src/modules/cars/useCases/listCategory/index.ts
@@ -2,9 +2,13 @@ import { CategoriesRepository } from "../../repositories/implementations/Categor
 import { ListCategoryController } from "./ListCategoryController";
 import { ListCategoryUseCase } from "./ListCategoryUseCase";
 
-const categoriesRepository = CategoriesRepository.getInstance();
-const listCategoryUseCase = new ListCategoryUseCase(categoriesRepository);
+export default (): ListCategoryController => {
+  const categoriesRepository = new CategoriesRepository();
+  const listCategoryUseCase = new ListCategoryUseCase(categoriesRepository);
 
-const listCategoryController = new ListCategoryController(listCategoryUseCase);
+  const listCategoryController = new ListCategoryController(
+    listCategoryUseCase
+  );
 
-export { listCategoryController };
+  return listCategoryController;
+};
