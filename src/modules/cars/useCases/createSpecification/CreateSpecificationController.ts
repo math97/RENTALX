cb53adda5cc6fc5d4fedb6faa2bbfad28ecd8f21
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2022-03-08 09:01:40 -0300

    modify to use tsrying on specification

--- src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import {
   CreateSpecificationUseCase,
@@ -6,12 +7,14 @@ import {
 } from "./CreateSpecificationUseCase";
 
 class CreateSpecificationController {
-  constructor(private createSpecificationUseCase: CreateSpecificationUseCase) {}
-
   handle(request: Request, response: Response): Response {
     const { name, description }: IRequest = request.body;
 
-    this.createSpecificationUseCase.execute({ name, description });
+    const createSpecificationUseCase = container.resolve(
+      CreateSpecificationUseCase
+    );
+
+    createSpecificationUseCase.execute({ name, description });
 
     return response.status(201).send();
   }
