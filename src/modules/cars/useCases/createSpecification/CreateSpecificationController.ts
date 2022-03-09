53e18adfb3d7f276c0bdbcc691acda85674dde4e
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2022-03-09 09:31:33 -0300

    adapt specification useCase to typeorm

--- src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -7,14 +7,14 @@ import {
 } from "./CreateSpecificationUseCase";
 
 class CreateSpecificationController {
-  handle(request: Request, response: Response): Response {
+  async handle(request: Request, response: Response): Promise<Response> {
     const { name, description }: IRequest = request.body;
 
     const createSpecificationUseCase = container.resolve(
       CreateSpecificationUseCase
     );
 
-    createSpecificationUseCase.execute({ name, description });
+    await createSpecificationUseCase.execute({ name, description });
 
     return response.status(201).send();
   }
