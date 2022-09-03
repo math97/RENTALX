15531a84dc3c5859590bca577d33672ca8bbdb35
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2022-09-03 22:15:46 +0100

    done UploadCarImages

---
+++ src/modules/cars/infra/typeorm/repositories/CarsImagesRepository.ts
@@ -0,0 +1,21 @@
+import { inject, injectable } from "tsyringe";
+import { getRepository, Repository } from "typeorm";
+
+import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+
+import { CarImage } from "../entities/CarImage";
+
+class CarsImagesRepository implements ICarsImagesRepository {
+  constructor(private repository: Repository<CarImage>) {
+    this.repository = getRepository(CarImage);
+  }
+  async create(car_id: string, image_name: string): Promise<CarImage> {
+    const carImage = this.repository.create({ car_id, image_name });
+
+    await this.repository.save(carImage);
+
+    return carImage;
+  }
+}
+
+export { CarsImagesRepository };
