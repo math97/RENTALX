73bada9f497111fd30b7ee21a8852046f5fc73da
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2022-09-18 20:46:05 +0100

    created first test for createRentalUseCase

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -0,0 +1,57 @@
+import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/RentalsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
+
+import { CreateRentalUseCase } from "./CreateRentalUseCase";
+
+let createRentalUseCase: CreateRentalUseCase;
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+
+describe("Create Rental", () => {
+  beforeEach(() => {
+    rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+    createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+  });
+
+  it("should be able to create a new rental", async () => {
+    const rental = await createRentalUseCase.execute({
+      car_id: "121212",
+      expected_return_date: new Date(),
+      user_id: "12345",
+    });
+
+    expect(rental).toHaveProperty("id");
+    expect(rental).toHaveProperty("start_date");
+  });
+
+  it("should not be able to create a new rental if there is another open to the same user", async () => {
+    expect(async () => {
+      const rental = await createRentalUseCase.execute({
+        car_id: "121212",
+        expected_return_date: new Date(),
+        user_id: "12345",
+      });
+
+      const rental2 = await createRentalUseCase.execute({
+        car_id: "121213",
+        expected_return_date: new Date(),
+        user_id: "12345",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to create a new rental if there is another open to the same car", async () => {
+    expect(async () => {
+      const rental = await createRentalUseCase.execute({
+        car_id: "121212",
+        expected_return_date: new Date(),
+        user_id: "12345",
+      });
+
+      const rental2 = await createRentalUseCase.execute({
+        car_id: "121212",
+        expected_return_date: new Date(),
+        user_id: "123456",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
