c82a0272a03b12e490008b2402af1c5c838ea7dd
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2022-09-18 20:03:30 +0100

    migration rental

--- src/shared/infra/typeorm/migrations/1647000845678-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1647000845678-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1647000845678 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
