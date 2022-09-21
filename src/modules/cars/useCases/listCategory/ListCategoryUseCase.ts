cb9f93113015e88595396dd0a1b2087edb43bd1f
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2022-09-21 18:42:22 +0100

    fix listcategory import error

--- src/modules/cars/useCases/listCategory/ListCategoryUseCase.ts
+++ src/modules/cars/useCases/listCategory/ListCategoryUseCase.ts
@@ -1,7 +1,6 @@
-import { response } from "express";
 import { inject, injectable } from "tsyringe";
 
-import { Category } from "@modules/cars/entities/Category";
+import { Category } from "@modules/cars/infra/typeorm/entities/Category";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 
 @injectable()
