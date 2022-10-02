ab795511720474e2678d7d36ffc3641dc531b785
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2022-10-02 21:45:55 +0100

    create a auth file

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "225639b3444e5150227e1f6b5d15f2a2",
+  expiresIn: "15m",
+  secret_refresh_token: "d24f1153e2c49374a5cb2ee50deffb66",
+  expiresIn_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
