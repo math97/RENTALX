b24427ca8f4e0aedbe997ce415d01d713dc4a8f9
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2022-10-03 12:56:42 +0100

    forgotemail

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,9 +1,11 @@
+import { resolve } from "path";
 import { inject, injectable } from "tsyringe";
 import { v4 as uuidV4 } from "uuid";
 
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
 import { AppError } from "@shared/errors/AppError";
 
 @injectable()
@@ -14,22 +16,45 @@ class SendForgotPasswordMailUseCase {
     @inject("UsersTokensRepository")
     private usersTokensRepository: IUsersTokensRepository,
     @inject("DayjsDateProvider")
-    private dayjsDateProvider: IDateProvider
+    private dayjsDateProvider: IDateProvider,
+    @inject("EtherealMailProvider")
+    private mailProvider: IMailProvider
   ) {}
   async execute(email: string): Promise<void> {
     const user = await this.usersRepository.findByEmail(email);
+
+    const templatePath = resolve(
+      __dirname,
+      "..",
+      "..",
+      "views",
+      "email",
+      "forgotPassword.hbs"
+    );
 
     if (!user) throw new AppError("User does not exists!");
 
     const token = uuidV4();
 
-    const expires = this.dayjsDateProvider.addHours(3);
+    const expires_date = this.dayjsDateProvider.addHours(3);
 
     await this.usersTokensRepository.create({
       refresh_token: token,
       user_id: user.id,
       expires_date,
     });
+
+    const variables = {
+      name: user.name,
+      link: `${process.env.FORGOT_MAIL_URL}${token}`,
+    };
+
+    await this.mailProvider.sendMail(
+      email,
+      "Recuperação de senha",
+      variables,
+      templatePath
+    );
   }
 }
 
