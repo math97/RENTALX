034dbaf7f27464ef83e95a44516103a052d8a3a8
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2022-10-14 19:45:52 +0100

    create userMap to not return all information from user

--- src/modules/accounts/useCases/profileUserUseCase/ProfileUserUseCase.ts
+++ src/modules/accounts/useCases/profileUserUseCase/ProfileUserUseCase.ts
@@ -1,6 +1,8 @@
 import { inject, injectable } from "tsyringe";
 
+import { IUserResponseDTO } from "@modules/accounts/dtos/IUserResponseDTO";
 import { User } from "@modules/accounts/infra/typeorm/entities/User";
+import { UserMap } from "@modules/accounts/mapper/UserMap";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 
 @injectable()
@@ -9,10 +11,10 @@ class ProfileUserUseCase {
     @inject("UsersRepository")
     private usersRepository: IUsersRepository
   ) {}
-  async execute(id: string): Promise<User> {
+  async execute(id: string): Promise<IUserResponseDTO> {
     const user = await this.usersRepository.findById(id);
 
-    return user;
+    return UserMap.toDTO(user);
   }
 }
 
